5a6d268050b17c09c3a03dfd2f91c91bffbe30d9
Lewis <Lewiswilson1497@gmail.com>
2022-02-09 13:43:34 +0000

    Template API with PetaPoco setup

---
+++ Contexts/DBContext.cs
@@ -0,0 +1,32 @@
+﻿using PetaPoco;
+using PetaPoco.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Template.Contexts {
+    public class DBContext : Database {
+
+        public DBContext(DbConnection connection, IMapper defaultMapper = null)
+            : base(connection, defaultMapper) {
+        }
+
+        public DBContext(string connectionString, string providerName, IMapper defaultMapper = null)
+            : base(connectionString, providerName, defaultMapper) {
+        }
+
+        public DBContext(string connectionString, DbProviderFactory factory, IMapper defaultMapper = null)
+            : base(connectionString, factory, defaultMapper) {
+        }
+
+        public DBContext(string connectionString, IProvider provider, IMapper defaultMapper = null)
+            : base(connectionString, provider, defaultMapper) {
+        }
+
+        public DBContext(IDatabaseBuildConfiguration configuration)
+            : base(configuration) {
+        }
+    }
+}
---
+++ Controllers/DatabaseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetaPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Template.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseController : ControllerBase {
+
+        private readonly IDatabase _context;
+
+        [HttpGet]
+        public IActionResult Get() {
+
+            var result = _context.Query<bool>("SELECT TRUE");
+            return Ok(result);
+
+        }
+
+        public DatabaseController(IDatabase context) {
+            _context = context;
+        }
+
+    }
+}
